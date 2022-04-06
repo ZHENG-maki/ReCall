86b6ed69ac889b318acd9491ed7ce3883dbdd779
郑宇键 <2082153514@qq.com>
2022-04-06 20:12:28 +0800

    初始版本，没有任何东西

---
+++ Source/ReCall/ReCallGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ReCallGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RECALL_API AReCallGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
