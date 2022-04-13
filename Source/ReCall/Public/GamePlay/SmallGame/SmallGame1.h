d7a0af4e565431205db9beaec28483c492c9fabc
zhengyujian <zhengyujian@dragonest.com>
2022-04-13 19:01:55 +0800

    开始解密开发
    添加解密1图片资源
    添加TestMap测试解密
    完成基础WBP_SmallGame1

---
+++ Source/ReCall/Public/GamePlay/SmallGame/SmallGame1.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "SmallGame1.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RECALL_API USmallGame1 : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
