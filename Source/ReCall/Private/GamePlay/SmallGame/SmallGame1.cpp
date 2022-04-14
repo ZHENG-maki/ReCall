231da82625fe7fe7a9714f236aa8911bc27918a8
zhengyujian <zhengyujian@dragonest.com>
2022-04-14 14:27:48 +0800

    第一个关卡实现
    在Map文件夹TestMap即可体验
    添加办公室门可互动版本

--- Source/ReCall/Private/GamePlay/SmallGame/SmallGame1.cpp
+++ Source/ReCall/Private/GamePlay/SmallGame/SmallGame1.cpp
@@ -2,4 +2,27 @@
 
 
 #include "GamePlay/SmallGame/SmallGame1.h"
+#include "Components/Button.h"
 
+void USmallGame1::NativeConstruct()
+{
+	
+}
+
+bool USmallGame1::CheckCurrentButtonAngle()
+{
+	if (
+		Button1->GetRenderTransformAngle() == 0 &&
+		Button2->GetRenderTransformAngle() == 0 &&
+		Button4->GetRenderTransformAngle() == 0 &&
+		(Button5->GetRenderTransformAngle() == 90 || Button5->GetRenderTransformAngle() == 180) &&
+		Button7->GetRenderTransformAngle() == 0 &&
+		(Button8->GetRenderTransformAngle() == 0 || Button8->GetRenderTransformAngle() == 180) &&
+		Button9->GetRenderTransformAngle() == 0
+		)
+	{
+		return true;
+	}
+
+	return false;
+}
--- Source/ReCall/Public/GamePlay/SmallGame/SmallGame1.h
+++ Source/ReCall/Public/GamePlay/SmallGame/SmallGame1.h
@@ -13,5 +13,30 @@ UCLASS()
 class RECALL_API USmallGame1 : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+public:
+	//绑定按钮
+	UPROPERTY(Meta = (BindWidgetOptional), BlueprintReadWrite)
+	class UButton* Button1;
+	UPROPERTY(Meta = (BindWidgetOptional), BlueprintReadWrite)
+	UButton* Button2;
+	UPROPERTY(Meta = (BindWidgetOptional), BlueprintReadWrite)
+	UButton* Button3;
+	UPROPERTY(Meta = (BindWidgetOptional), BlueprintReadWrite)
+	UButton* Button4;
+	UPROPERTY(Meta = (BindWidgetOptional), BlueprintReadWrite)
+	UButton* Button5;
+	UPROPERTY(Meta = (BindWidgetOptional), BlueprintReadWrite)
+	UButton* Button6;
+	UPROPERTY(Meta = (BindWidgetOptional), BlueprintReadWrite)
+	UButton* Button7;
+	UPROPERTY(Meta = (BindWidgetOptional), BlueprintReadWrite)
+	UButton* Button8;
+	UPROPERTY(Meta = (BindWidgetOptional), BlueprintReadWrite)
+	UButton* Button9;
+public:
+	virtual void NativeConstruct() override;
+
+	UFUNCTION(BlueprintCallable)
+	bool CheckCurrentButtonAngle();
 };
